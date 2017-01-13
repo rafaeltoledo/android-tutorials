c436afaeb996748046ae899a0ad41daa3e30be04
Rafael Toledo <rafaeldtoledo@gmail.com>
2017-01-13 00:30:52 -0200

    Android Tutorial #4 - Layouts & Resources

--- app/src/main/java/net/rafaeltoledo/tutorial/MainActivity.java
+++ app/src/main/java/net/rafaeltoledo/tutorial/MainActivity.java
@@ -1,21 +1,14 @@
 package net.rafaeltoledo.tutorial;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        Log.d("MA::Action", intent.getAction());
-        for (String category : intent.getCategories()) {
-            Log.d("MA::Category", category);
-        }
-        Log.d("MA::Component", intent.getComponent().getClassName());
+        setContentView(R.layout.activity_main);
     }
 }
